4a44721d2ee04fa5376baa3d876560300bee9eff
Camilo Cuervo <167337666+elcuervazo@users.noreply.github.com>
2024-04-25 09:58:46 -0500

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-name=(input("digite el nombre del estudiante:" ))
-# la variable (name) almacena y permite introducir el nombre del estudiante segun lo indica la actividad
-ins=(input("digite en que institucion estudia:" ))
-# la variable (ins) almacena el colegio o instituto en el que estudia el estudiante
-grade=(input("digite el grado:" ))
-# la variable (grade) almacena el grado que cursa
-print("el estudiante" ,name , "estudia en el" ,ins , "y cursa el tecnologo de" ,grade, )
-# el printe permite obtener la salida el texto requerido
