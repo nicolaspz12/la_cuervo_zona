2f559861cc73264869a28b1e453537b17fe58be5
Camilo Cuervo <167337666+elcuervazo@users.noreply.github.com>
2024-04-25 09:53:29 -0500

    Update Ejercicio5.py

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,4 +1,10 @@
-value=int(input("Valor de la compra"))
-dicount=int(input("Valor del desucento"))
-total_pay= (value-dicount)
-
+value=float(input("digite el valor de su compra:"))
+#(value) almacenas el valor de la compra
+discount=float(input("digite el valor del descuento (%):"))
+#(discount) almacena el valor del descuento
+discount_value= value * (discount/100)
+#(discount_value) permite entontrar el descuento
+total_value=value-discount_value
+# da el precio total
+print("la compra fue", value, ",con un descuento de", discount_value, ", y el Valor final a pagar:", total_value, )
+#imprime el resultado deseado segun la guia
