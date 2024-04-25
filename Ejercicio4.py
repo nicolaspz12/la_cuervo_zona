8b2a01e51a3a3d1b2e50de5e58cb55a7f289cd97
Camilo Cuervo <167337666+elcuervazo@users.noreply.github.com>
2024-04-25 09:59:31 -0500

    Delete Ejercicio4.py

--- Ejercicio4.py
+++
@@ -1,9 +0,0 @@
-name=(input("ingrese el nombre del vendedor:"))
-#la variable (name) almacena el nombre dek vendedor 
-basspay=int(input("ingrese el sueldo base:"))
-# la variable (basspay) define el sueldo del vendedor
-tip=int(input("ingrese el valor de las comisiones:"))
-# la variable (tip) guarda el valor agregado de comisiones
-total_pay= (basspay+tip)
-# suma los valores (basspay,tip) dandonos el valor final
-print("el vendedor" ,name , ", tiene un sueldo base de" ,basspay , ", durante el mes obtuvo una comision de" ,tip , "y el sueldo final es:" ,total_pay , )
