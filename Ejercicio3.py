4a81a14b1896f0e4eab52233f03c315f58d3fdea
Camilo Cuervo <167337666+elcuervazo@users.noreply.github.com>
2024-04-25 09:59:20 -0500

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("Digite el primer valor:"))
-# esta variable almacena el primer digito ingresado
-num2=int(input("Digite el segundo valor:"))
-# esta variable almacena el segundo digito ingresado
-num3=int(input("Digite el tercer valor:"))
-# esta variable almacena el tercer digito ingresado
-prom= (num1+num2+num3)/3
-#la variable (prom) guarda la formula del promedio la cual es la sumna de los 3 numeros ingresados, dividido por la cantidad de numero ingresados, en este caso es 3
-print("El promedio de los numeros ingresados es",prom)
