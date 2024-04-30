b30a28fc8352760806a9b7cae8cff338a2767c97
Nicolas plazas <167337533+nicolaspz12@users.noreply.github.com>
2024-04-29 20:39:10 -0500

    Update ejercicio1.py

--- Ejercicios 1/ejercicio1.py
+++ Ejercicios 1/ejercicio1.py
@@ -1,16 +1,16 @@
-a=2
-b=5
+a=1
+b=1
 suma=a+b
 print("la suma es: ",suma)
-a=2
-b=5
+a=1
+b=1
 resta=a-b
 print("la resta es : ",resta)
-a=2
-b=5
+a=1
+b=1
 multiplicacion=a*b
 print("producto : ",multiplicacion)
-a=2
-b=5
+a=1
+b=1
 divisionresiduo=a%b
 print("el residuo es : ",divisionresiduo)
